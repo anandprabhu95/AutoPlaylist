819e64aedf15b8f62f14d52561f1b1259688ac78
Anand Prabhu <anandprabhu95@outlook.com>
2023-07-09 02:08:09 +0200

    Add bash script for crontab

--- spotipy_creds.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo Exporting spotify credentials as environment variables
-export SPOTIPY_CLIENT_ID="Your Client_Id"
-export SPOTIPY_CLIENT_SECRET="Your Client_Secret"
-export SPOTIPY_REDIRECT_URI="Redirect uri"
-
